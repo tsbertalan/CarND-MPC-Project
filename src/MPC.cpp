9aafba6e56e8723cdd0ac7941558f3a56a9b4b10
Tom Bertalan <tom@tombertalan.com>
2018-05-09 22:02:04 -0400

    Max speed 101 without latency

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -18,12 +18,12 @@ static const size_t N = 20;
 static const double dt = 0.06;
 
 // Set the target velocity (converted from mph to m/s)
-static const double ref_v = 90 * 5280. / 3.2808 / 3600.;
+static const double ref_v = 150 * 5280. / 3.2808 / 3600.;
 
 // Set the scaling factors for terms in the objective function.
-static const double scale_cte = 30;
-static const double scale_epsi = 100;
-static const double scale_v = .08;
+static const double scale_cte = 100;
+static const double scale_epsi = 600;
+static const double scale_v = .2;
 static const double scale_delta = 1e-5;
 static const double scale_a = .1;
 static const double scale_ddelta = 1e11;
