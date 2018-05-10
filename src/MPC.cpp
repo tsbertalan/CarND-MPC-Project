72e44b65d6dafca61d6f9496b6a4d7bfc2ef8039
Tom Bertalan <tom@tombertalan.com>
2018-05-10 11:00:12 -0400

    Sort-of ok at a higher target speed

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -15,19 +15,19 @@ using std::cout;
 
 // Set the timestep length and duration
 static const size_t N = 10;
-static const double dt = 0.16;
+static const double dt = 0.12;
 
 // Set the target velocity (converted from mph to m/s)
 static const double ref_v = 120 * 5280. / 3.2808 / 3600.;
 
 // Set the scaling factors for terms in the objective function.
-static const double scale_cte = 250;
-static const double scale_epsi = 500;
-static const double scale_v = .5;
-static const double scale_delta = 1e-5;
-static const double scale_a = .1;
-static const double scale_ddelta = 1e12;
-static const double scale_da = 1;
+static const double scale_cte = 500;
+static const double scale_epsi = 1000;
+static const double scale_v = 1;
+static const double scale_delta = 100;
+static const double scale_a = 10;
+static const double scale_ddelta = 1e8;
+static const double scale_da = 1e6;
 
 
 // If debugging the MPC solver33, set print_level to "4".
